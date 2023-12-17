c5cf966f9fd19f413a761edd79fe1f1bb1b86654
rajchatterz <iamraj.space@gmail.com>
2023-12-17 12:13:18 +0530

    start creating a bisc form using react native

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import {View, Text, Button, TextInput, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+
+const App = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+export default App;
